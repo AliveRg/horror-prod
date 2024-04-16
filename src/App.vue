a037652dccce5b3ebb9032771684cd47787504a3
AliveRg <renatgorodov49280@gmail.com>
2024-04-16 18:37:25 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header class="">
+    <div
+      class="conatiner bg-[#202020] pt-[24px] pb-[42px] px-[64px] flex items-center justify-between"
+    >
+      <img alt="Vue logo" class="logo w-[234px]" src="@/assets/Logo.svg" width="125" height="125" />
+
+      <ul class="flex items-center justify-end gap-[96px]">
+        <li class="text-white text-[28px] font-semibold font-montser leading-loose">квесты</li>
+        <li class="text-white text-[28px] font-semibold font-montser leading-loose">о нас</li>
+        <li class="text-white text-[28px] font-semibold font-montser leading-loose">контакты</li>
+      </ul>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,79 @@
+<script setup>
+import Container from 'postcss/lib/container'
+</script>
+
+<template>
+  <main class="bg-[#202020]">
+    <section>
+      <div
+        class="container-bg w-screen h-[641px] pt-[48px] pl-[64px] pr-[76px] pb-[83px] flex flex-col justify-between items-baseline"
+      >
+        <div class="w-[440px] h-[236px] font-montser font-medium leading-[80px] text-[64px]">
+          <span class="text-white">THE </span>
+          <span class="text-red-500 font-black">BEST<br /></span>
+          <p class="text-white">QUEST</p>
+          <span class="text-white">IN </span>
+          <span class="text-red-500 font-black">MOSCOW</span>
+        </div>
+        <div class="w-[595px] h-[194px] text-right font-medium self-end text-[32px]">
+          <span class="text-white font-montser">Мы работаем над каждым </span
+          ><span class="text-rose-400 font-montser">квестом</span
+          ><span class="text-white font-montser">, чтобы создать атмосферу настоящего</span
+          ><span class="text-white font-semibold font-montser"> </span
+          ><span class="text-rose-500 font-semibold font-montser">ужаса</span
+          ><span class="text-white font-montser"> и заставить участников испытать все </span
+          ><span class="text-red-500 font-bold font-montser">эмоции</span
+          ><span class="text-white font-montser">, от </span
+          ><span class="text-red-600 font-extrabold font-montser">страха</span
+          ><span class="text-white font-montser"> до восторга.</span>
+        </div>
+      </div>
+    </section>
+    <section>
+      <div class="w-screen pt-[48px] px-[64px] flex flex-col justify-between items-baseline">
+        <p class="text-white text-[64px] font-black font-montser leading-[80px]">Квесты</p>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cuests: [
+        {
+          name: 'Синистер',
+          price: 6000,
+          peopleCount: 4
+        },
+        {
+          name: 'Шесть демонов Эмили Роуз',
+          price: 6000,
+          peopleCount: 4
+        },
+        {
+          name: 'Крик',
+          price: 6000,
+          peopleCount: 4
+        },
+        {
+          name: 'Сплит',
+          price: 6000,
+          peopleCount: 4
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.container-bg {
+  background-image: url(@/assets/imagehero.png);
+  background-repeat: no-repeat;
+  background-position: center center;
+  background-size: cover;
+  /* background-attachment: fixed; */
+}
+</style>
